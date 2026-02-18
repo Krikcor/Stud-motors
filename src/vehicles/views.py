bbbeeb88537f4bb5fae47d8b10041da53b392257
Vahnky <vahnky@gmail.com>
2026-02-18 15:32:26 +0100

    Initialisation projet django

---
+++ src/vehicles/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def vehicle_list(request):
+    return render(request, 'vehicles/vehicle_list.html')
+from django.shortcuts import render
+
+# Create your views here.
