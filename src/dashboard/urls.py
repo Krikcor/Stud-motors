8e3d3e9810adbbd1611c695c1c6ce00436be9fa2
Vahnky <vahnky@gmail.com>
2026-02-24 22:12:32 +0100

    finalisation de la branche vehicles pro dashboard

--- src/dashboard/urls.py
+++ src/dashboard/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path("vehicles/create/", views.create_vehicle, name="create_vehicle"),
     path("vehicle/delete/", views.delete_vehicle, name="delete_vehicle"),
     path("vehicle/modify/", views.modify_vehicle, name="modify_vehicle"),
-    path("logout/", LogoutView.as_view(next_page="home"), name="logout"),
+    path("logout/", LogoutView.as_view(next_page="index"), name="logout"),
     path("vehicle/list/", views.list_vehicle, name="list_vehicle"),
 ]
