0219ce29a783aa2fd723284579d03fd610a0a257
Vahnky <vahnky@gmail.com>
2026-02-24 22:52:09 +0100

    affichage des vehicles dans des cards cliquables

--- src/vehicles/views.py
+++ src/vehicles/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Vehicle
+from django.contrib.auth.decorators import login_required
+
 
 def vehicle_list(request):
-    return render(request, 'vehicles/vehicle_list.html')
-from django.shortcuts import render
+    vehicles = Vehicle.objects.all().order_by("-created_at")
+    return render(request, "vehicles/vehicle_list.html", {
+        "vehicles": vehicles
+    })
 
-# Create your views here.
+
+def vehicle_detail(request, pk):
+    vehicle = get_object_or_404(Vehicle, pk=pk)
+    return render(request, "vehicles/vehicle_detail.html", {
+        "vehicle": vehicle
+    })
